834943fec46a89bab8138c4bee41ff5713c3cf04
YakYakYakYakYakDT <chuajunjiejeremy@gmail.com>
2021-08-23 23:22:10 +0800

    install ejs, converted to working on localhost, broke homepage background

---
+++ routes/main.js
@@ -0,0 +1,13 @@
+module.exports = function(app) {
+    app.get("/",function(req, res){
+        res.render("homepage.html")
+        });
+    
+    app.get("/login",function(req, res) {
+        res.render("login.html");
+        });
+    
+    app.get("/signup",function(req, res) {
+        res.render("signup.html");
+        });
+    }
