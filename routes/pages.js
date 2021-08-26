c31e06d01e0965f607e27e216b28021f46d14c68
YakYakYakYakYakDT <chuajunjiejeremy@gmail.com>
2021-08-26 12:45:39 +0800

    query db for browse page, can grab results, now to work on displaying to browse.hbs

--- routes/pages.js
+++ routes/pages.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const authController = require('../controllers/auth');
 const router = express.Router();
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
 
 router.get("/", authController.isLoggedIn, (req,res)=> {
     res.render("index", {
@@ -29,16 +37,16 @@ router.get('/profile', authController.isLoggedIn, (req, res) => {
     }
 });
 
-//check token, render token else redirect to login.
-router.get('/browse', authController.isLoggedIn, (req, res) => {
+router.get('/browse', (req, res) => {
 
-    if(req.user) {
-        res.render('browse', {
-            user:req.user
-        });
-    } else {
-        res.redirect('/login');
-    }
-});
+    db.query("SELECT * FROM users", (error, result) => {
+        if(error) {
+            res.redirect('/');
+        }
+        console.log('test');
+        console.log(result);
+        res.render('browse', {result});
+    })            
+})
 
 module.exports = router;
