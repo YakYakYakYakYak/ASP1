2e42ff338ce65ff6b812974a4d086593ef861623
YakYakYakYakYakDT <chuajunjiejeremy@gmail.com>
2021-09-03 16:03:56 +0800

    reset app.js

--- app.js
+++ app.js
@@ -26,14 +26,14 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.set('view engine', 'hbs');
-/*
+
 db.connect( (error) => {
     if(error) {
         console.log(error)
     } else {
         console.log("MYSQL Connected...")
     }
-})*/
+})
 //Define routes
 app.use('/', require('./routes/pages'));
 app.use('/auth', require('./routes//auth'));
@@ -41,24 +41,3 @@ app.use('/auth', require('./routes//auth'));
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server started on port: " + portNumber);
 })
-
-function handleDisconnect() {
-  
-  db.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    }                                     // to avoid a hot loop, and to allow our node script to
-  });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
-  db.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
-  });
-}
-
-handleDisconnect();
